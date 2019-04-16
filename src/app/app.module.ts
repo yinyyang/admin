ed76d476140536d331ab6ca33349fc8ba21d5c1f
Tiny <tong.tong1026@163.com>
2019-04-16 21:54:02 +0800

    add basic main page layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {LoginModule} from './login/login.module';
 import {MainModule} from './main/main.module';
+import { HeaderComponent } from './main/header/header.component';
+import { SidebarComponent } from './main/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -4,9 +4,12 @@ import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import {MainComponent} from './main.component';
 import {MainGuard} from './main.guard';
+import {HeaderComponent} from './header/header.component';
+import {SidebarComponent} from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [MainComponent, HeaderComponent, SidebarComponent, FooterComponent],
   imports: [
     CommonModule,
     MainRoutingModule
