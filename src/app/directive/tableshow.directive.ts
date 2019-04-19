b91e2857ce8e8b6d351527e1cfb3ed2af5da8b85
Tiny <tong.tong1026@163.com>
2019-04-19 22:16:15 +0800

    change tableshow directive to general

--- src/app/directive/tableshow.directive.ts
+++ src/app/directive/tableshow.directive.ts
@@ -1,16 +1,19 @@
-import {Directive, ElementRef, HostListener, Input, Output} from '@angular/core';
+import {Component, Directive, ElementRef, HostListener, Input, Output, EventEmitter} from '@angular/core';
 import {tap} from 'rxjs/internal/operators';
 
 @Directive({
   selector: '[appTableshow]'
 })
 export class TableshowDirective {
+
   @Input()
-  @Output()
-  private isShow: boolean;
+  tableId: string;
+
+  isShow: boolean;
 
 
   constructor(private el: ElementRef) {
+    this.isShow = true;
   }
 
   @HostListener('click') onClick() {
@@ -19,11 +22,12 @@ export class TableshowDirective {
 
   private hide() {
     if (this.isShow === true) {
-      document.getElementById('userInfo').style.display = 'none';
+
+      document.getElementById(this.tableId).style.display = 'none';
       this.isShow = false;
       this.el.nativeElement.getElementsByTagName('i').item(0).setAttribute('class', 'glyphicon glyphicon-menu-up');
     } else {
-      document.getElementById('userInfo').removeAttribute('style');
+      document.getElementById(this.tableId).removeAttribute('style');
       this.isShow = true;
       this.el.nativeElement.getElementsByTagName('i').item(0).setAttribute('class', 'glyphicon glyphicon-menu-down');
     }
