090327a92627dcdde1ea2dbd58be919bb2bcb302
tiny <tong.tong1026@163.com>
2019-05-16 10:21:21 +0800

    animation components

---
+++ src/app/anminations/css3loading/css3loading.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-css3loading',
+  templateUrl: './css3loading.component.html',
+  styleUrls: ['./css3loading.component.css']
+})
+export class Css3loadingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/anminations/gifloading/gifloading.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gifloading',
+  templateUrl: './gifloading.component.html',
+  styleUrls: ['./gifloading.component.css']
+})
+export class GifloadingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
