6b3ae6278e1c7f312d6b12ab0552e00eba095d76
tiny <tong.tong1026@163.com>
2019-06-14 17:34:38 +0800

    add side-bar style

--- src/app/main/sidebar/sidebar.component.ts
+++ src/app/main/sidebar/sidebar.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+   menuList:any;
+   selected:any;
+  constructor() {
+    this.menuList = [
+      {
+        "name": "Angular",
+        "subMenu": ["Anguler 1", "Angular 2"]
+      },
+      {
+        "name": "Javascript",
+        "subMenu": ["Jquery", "Ajax"]
+      },
+      {
+        "name": "Bootstrap",
+        "subMenu": ["BootStrap 2", "BootStrap 3"]
+      }
+     ]
 
-  constructor() { }
+  }
 
   ngOnInit() {
   }
 
+  select(item){
+
+    this.selected = (this.selected === item ? null : item);
+  }
+  isActive(item){
+    return this.selected === item;
+  }
 }
